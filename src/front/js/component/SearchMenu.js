f1dfeefdd0f74c46c368930e59e78b1966878e21
Programisto1011 <victormaynougomez@gmail.com>
2022-02-22 13:57:14 +0000

    Alpha version Search Menu

--- src/front/js/component/SearchMenu.js
+++ src/front/js/component/SearchMenu.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, ImageBackground } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import image from "../../img/Vector.png";
+
 import * as PropTypes from "prop-types";
 import "../../styles/SearchMenu.css"
 
@@ -17,39 +20,55 @@ import "../../styles/SearchMenu.css"
 // npm i --save @fortawesome/pro-solid-svg-icons
 
 const SearchMenu = () => (
-	<div className="container SearchMenu p-3">
-        <Row className="containerButton m-3">
-            <Col xs={6} md={2} lg="1" className="text-left">
-                <Button variant="primary justify-content-left buttonSearchMenu">Buy</Button>
-            </Col>
-            <Col xs={6} md={2}>
-                <Button variant="primary">Sell</Button>
-            </Col>
-        </Row>
-        <Row xs={1} md={6} lg={6} className="justify-content-left m-3 rowContainer">
-            <Col xs={12} md={5} lg={4} className="mt-2">
-                <Form.Select aria-label="Default select example">
-                    <option>Properties Type</option>
-                    <option value="1">Flat</option>
-                    <option value="2">Detached</option>
-                </Form.Select>
-            </Col>
-            <Col xs={12} md="auto" lg={8} className="mt-2 mb-2">
-                <InputGroup >
-                    <InputGroup.Text className="inputTransparent" id="basic-addon1">
-                        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                    </InputGroup.Text>
-                    <FormControl className="inputTransparent"
-                    placeholder="Search by location"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                    />
-                </InputGroup>              
-            </Col>
-            <Col xs={12} md={2} lg="1" className="mt-2">
-                <Button variant="primary justify-content-left buttonSearchMenu">Search</Button>
-            </Col>
-        </Row>
+
+	<div>
+        <div className="container header" style={{backgroundImage: `url(${image}`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat' }}>
+            <Row  className="justify-content-center search-menu-header text-header-search-menu" >
+                <Col>
+                    <p> Piso Fijo</p>
+                </Col>
+            </Row>
+        </div>
+        <div className="container SearchMenu p-3">
+            <Row xs={12} md={6} lg={2} className="containerButton m-3">  
+                <div class="button buttonSelect">
+                    <input type="radio" id="a25" name="check-substitution-2" />
+                    <label class="btn btn-default " for="a25">Buy</label>
+                </div>
+                <div class="button buttonSelect">
+                    <input type="radio" id="a50" name="check-substitution-2" />
+                    <label class="btn btn-default" for="a50">Sell</label>
+                </div>
+            </Row>
+            <Row xs={1} md={6} lg={6} className="justify-content-left m-3 rowContainer">
+                <Col xs={12} md={5} lg={2} className="mt-2">
+                    <Form.Select aria-label="Default select example" className="styleSelect">
+                        <option>Properties Type</option>
+                        <option value="1">Flat</option>
+                        <option value="2">Detached</option>
+                    </Form.Select>
+
+                </Col>
+                <Col xs={12} md="auto" lg={8} className="mt-2 mb-2">
+                    <InputGroup >
+                        <InputGroup.Text className="inputTransparent" id="basic-addon1">
+                            <FontAwesomeIcon icon={ faMagnifyingGlass } className="icon"/>
+                        </InputGroup.Text>
+                        <FormControl className="inputTransparent"
+                        placeholder="Search by location"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col xs={12} md={2} lg={2} className="mt-2">
+                    <Button variant="primary justify-content-left buttonSearchMenu" className="buttonSearch mb-1">Search</Button>
+                </Col>
+            </Row>
+        </div>
 	</div>
 );
 
