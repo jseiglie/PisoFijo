629491e3a2a9d96f07a62669125c30d02fbec5bb
jseiglie <javierseiglie@gmail.com>
2022-02-23 15:47:00 +0000

    base created

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import ButtonOwn from "../component/Button.js";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const login = () => {
+    return ()
+}
+
+export default login;
