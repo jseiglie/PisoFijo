8a3b27961a283f3c928d5b2cd37e6aa240c85d56
jseiglie <javierseiglie@gmail.com>
2022-02-23 10:30:46 +0000

    small details section added

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,16 +3,50 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import ButtonOwn from "../component/Button.js";
-import SearchMenu from "../component/SearchMenu.js"
-
+import SearchMenu from "../component/SearchMenu.js";
+import banner from "../../img/banner.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
-      <SearchMenu />
-     
-    </div>
+    <>
+      <div className="text-center mt-5">
+        <SearchMenu />
+      </div>
+      <div className="container">
+        <div
+          style={{
+            backgroundImage: `url(${banner}`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+          >
+          <Row className="section-margin">
+          <Col xs={12} md={4}>
+            <div className="card-body smDetails">
+              <p>lorem ipsum </p>
+              <ButtonOwn title={"More"} />
+            </div>
+          </Col>
+          <Col xs={12} md={4}>
+            <div className="card-body smDetails">
+              <p>lorem ipsum </p>
+              <ButtonOwn title={"More"} />
+            </div>
+          </Col>
+          <Col xs={12} md={4}>
+            <div className="card-body smDetails">
+              <p>lorem ipsum </p>
+              <ButtonOwn title={"More"} />
+            </div>
+          </Col>
+      </Row>
+        </div>
+        </div>
+    </>
   );
 };
