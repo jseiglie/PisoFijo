23ecd09af91680c984ca1dc64964f7e911d25285
Programisto1011 <victormaynougomez@gmail.com>
2022-02-24 10:58:42 +0000

    Alpha version FilterMenu

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,7 +4,6 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import ButtonOwn from "../component/Button.js";
 import SearchMenu from "../component/SearchMenu.js"
-import FilterMenu from "../component/FilterMenu.js"
 import banner from "../../img/banner.png";
 import servMap from "../../img/servMap.png";
 import servAR from "../../img/servAR.png";
@@ -19,7 +18,6 @@ export const Home = () => {
     <>
       <div className="searchSection text-center mt-5">
         <SearchMenu />
-        <FilterMenu /> 
       </div>
       <div className="container">
         <div
