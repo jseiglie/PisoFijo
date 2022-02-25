30e81adb3388385d337319a0040c70cefe35a6a5
Programisto1011 <victormaynougomez@gmail.com>
2022-02-25 11:51:27 +0000

    Correct responsive of detail img

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -80,76 +80,69 @@ export const Home = () => {
           </Row>
         </div>
       </div>
-
-
       <div className="servSection">
         <div className="container ">
-        <Row xs={8} md={8} lg={8} className="infoCard">
-          <Col md={2}></Col>
-
-          <Col xs={12} md={4}>
-            <div className="container ">
-              <img className="imgMap" src={`${servMap}`}></img>
-            </div>
-          </Col>
-          <Col xs={12} md={4}>
-            <p className="smDetails ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </Col>
-          <Col md={2}></Col>
-        </Row>
-        <Row>
-          <Col md={1}></Col>
-          <Col md={6}>
-            <div className="container moreDetailHead">
-              <div
-                style={{
-                  backgroundImage: `url(${servRoom})`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  border: '2px solid #F7BE53'
-                }}
-              >
-                <p className="smDetailsV pTextDetails">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-                </p>
+          <Row xs={12} md={12} lg={12} xl={12} className="infoCard">
+            <Col xs={12} md={12} lg={5} xl={5}>
+              <div className="container ">
+                <img className="imgMap" src={`${servMap}`}></img>
               </div>
-            </div>
-          </Col>
-          <Col md={5}>
-            <div className="container smDetailsV moreDetailHead infoCard">
-              <div className="text-center">
-                <img className="imgAR" src={`${servAR}`}></img>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            </Col>
+            <Col xs={12} md={12} lg={7} xl={7}>
+              <p className="smDetails ">
+                1- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum
               </p>
-            </div>
-          </Col>
-         
-        </Row>
-      </div>
+            </Col>
+          </Row>
+          <Row xs={12} md={12} lg={12} xl={12} className="d-flex">
+            <Col xs={12} md={12} lg={8} xl={8} className="g-0">
+              <div className="container moreDetailHead d-flex">
+                <div
+                  style={{
+                    backgroundImage: `url(${servRoom})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    border: '2px solid #F7BE53'
+                  }}
+                >
+                  <p className="smDetailsV pTextDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} md={12} lg={4} xl={4} className="d-flex g-0">
+              <div className="container smDetailsV moreDetailHead infoCard">
+                <div className="text-center">
+                  <img className="imgAR" src={`${servAR}`}></img>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     </>
   );
