64cc949ff32122e8d8c0ef530a56b3d4ff15b306
Programisto1011 <victormaynougomez@gmail.com>
2022-03-03 17:03:01 +0000

    Doing property table

--- src/api/models.py
+++ src/api/models.py
@@ -25,3 +25,47 @@ class User(db.Model):
             "telephone": self.telephone,
             # do not serialize the password, its a security breach
         }
+
+class Property(db.Model):
+    __tablename__ = 'property'
+    propertyCode = db.Column(db.Integer, primary_key=True)
+    ownerId = db.Column(Integer, ForeignKey('user.id'))
+    address = db.Column(db.String(120), unique=False, nullable=True)
+    agency = db.Column(db.Boolean, unique=False, nullable=True)
+    bathrooms = db.Column(db.Integer(2), unique=False, nullable=True)
+    condition = db.Column(db.String(10), unique=False, nullable=True)
+    description = db.Column(db.String(1500), unique=False, nullable=True)
+    distance = db.Column(db.String(20), unique=False, nullable=True)
+    district = db.Column(db.String(120), unique=False, nullable=True)
+    floor = db.Column(db.String(10), unique=False, nullable=True)
+    latitude = db.Column(db.Float(20), unique=False, nullable=True) #<--------------- 40.4962591
+    longitude = db.Column(db.Float(20), unique=False, nullable=True) #<--------------- -3.6561735
+    municipality = db.Column(db.String(20), unique=False, nullable=True)
+    operation = db.Column(db.String(20), unique=False, nullable=True)
+    price = db.Column(db.Integer(20), unique=False, nullable=True)
+    propertyType = db.Column(db.String(20), unique=False, nullable=True)
+    province = db.Column(db.String(20), unique=False, nullable=True)
+    rooms = db.Column(db.Integer(2), unique=False, nullable=True)
+    size = db.Column(db.Integer(5), unique=False, nullable=True)
+    bathrooms = db.Column(db.Integer(2), unique=False, nullable=True)
+
+    elevator = db.Column(db.Boolean, unique=False, nullable=True)
+    bankOffer = db.Column(db.String(20), unique=False, nullable=True)
+    preservation = db.Column(db.String(20), unique=False, nullable=True)
+
+    contact_Name = db.Column(db.String(20), unique=False, nullable=True)
+    contact_Phone = db.Column(db.String(20), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.userName
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userName": self.userName,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "email": self.email,
+            "telephone": self.telephone,
+            # do not serialize the password, its a security breach
+        }
