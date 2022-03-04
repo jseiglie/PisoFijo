7f90734c96e89cd8958887b9a5c730d23b867cb6
jseiglie <javierseiglie@gmail.com>
2022-03-04 17:04:58 +0000

    terminar de hacer seed data a partir de models.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,7 +8,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Property
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
---
+++ src/api/seed_data.py
@@ -0,0 +1,218 @@
+data ={
+    'User': [
+        {
+            "id": 1,
+            "firstName": 'Javier',
+            "lastName": 'Leyva Seiglie',
+            "email": 'javierseiglie@gmail.com',
+            "telephone": 722390929,
+            "properties": ''
+        },
+        {
+            "id": 2,
+            "firstName": 'Victor',
+            "lastName": 'Victor Victor',
+            "email": 'Victor@gmail.com',
+            "telephone": 722666666,
+            "properties": ''
+        },
+        {
+            "id": 3,
+            "firstName": 'Peter',
+            "lastName": 'La Anguila',
+            "email": 'anguila@gmail.com',
+            "telephone": 6969696969,
+            "properties": ''
+        },
+        
+        {
+            "id": 4,
+            "firstName": 'Yusimi',
+            "lastName": 'Danger Flores',
+            "email": 'layusi@gmail.com',
+            "telephone": 6777677767,
+            "properties": ''
+        },
+    ],
+    [
+        {
+            "propertyCode": "96901409",
+            "thumbnail": "https://img3.idealista.com/blur/WEB_LISTING/0/id.pro.es.image.master/58/45/4d/960012591.jpg",
+            "numPhotos": 52,
+            "floor": "2",
+            "price": 1590000,
+            "propertyType": "flat",
+            "operation": "sale",
+            "size": 146,
+            "rooms": 2,
+            "bathrooms": 3,
+            "address": "barrio Recoletos",
+            "province": "Madrid",
+            "municipality": "Madrid",
+            "district": "Barrio de Salamanca",
+            "latitude": 40.4292726,
+            "longitude": -3.6839874,
+            "distance": "1526",
+            "description": "PROMORA MADRID vende piso reformado en Recoletos de 146m², 2 dormitorios los dos en suite más aseo de cortesía. En pleno barrio de Recoletos situado en una finca de principios de siglo nos encontramos esta vivienda situada en una segunda planta.  La vivienda que actualmente se encuentra reformada se distribuye en un vestíbulo con acceso a un aseo de cortesía, A continuación, nos encontramos los dormitorios los dos en suite. La cocina de diseño con una gran isla y equipada con electrodomésticos de primera gama.  Por último, un amplio salón- comedor con tres grandes ventanales.  A destacar su luminosidad, su tranquilidad y los grandes espacios. La vivienda cuenta con calefacción central, aire acondicionado y servicio de portería. MUCHAS DE NUESTRAS PROPIEDADES NO APARECEN EN IDEALISTA, CONSULTA NUESTRA PÁGINA WEB PARA VER MÁS PROPIEDADES EXCLUSIVAS. Sin duda, el distrito selecto por excelencia de Madrid. Pasear por las calles del barrio de Salamanca es contemplar los edificios más elegantes y codiciados de la capital: construcciones históricas de portales señoriales e interiores amplios y altos. Concebido en las últimas décadas del siglo XIX por D. José de Salamanca y Mayol, Marqués de Salamanca, este ensanche de la ciudad conserva intacto su aire aristocrático, que podemos ver en palacetes como el de la Embajada de Italia, por ejemplo. En este epicentro del lujo se localiza la conocida “Milla de Oro”: un área comercial donde tienen su sede marcas prestigiosas como Dior, Chanel, Prada o Tiffany y Promora. Tiendas como Gallery, BD o Vitra y restaurantes como Ramsés o No, responden a los gustos de un life style moderno y sofisticado. Hablamos de una zona dinámica y segura, de grandes aceras, con un parking casi ilimitado y con una interesante vida nocturna: Nxt Club, Déjate Besar o Le Boutique Club. No faltan las galerías de arte y los espacios dedicados a la cultura como la Fundación March o el museo Lázaro Galdiano. Sin olvidar sus árboles frondosos y la cercanía al parque del Retiro, el gran pulmón verde del centro de Madrid.",
+            "status": "good",
+            "hasLift": true,
+            "agency" : null,
+            "bankOffer" : null,
+            "ownerId" : null,
+            "contact_Name" : null,
+            "contact_Phone" : null
+        },
+         {
+            "propertyCode": "90075076",
+            "thumbnail": "https://img3.idealista.com/blur/WEB_LISTING/0/id.pro.es.image.master/8a/72/25/953062083.jpg",
+            "externalReference": "V6453I",
+            "numPhotos": 69,
+            "price": 9000000,
+            "propertyType": "chalet",
+            "operation": "sale",
+            "size": 2000,
+            "exterior": false,
+            "rooms": 8,
+            "bathrooms": 2,
+            "address": "Calle del Camino Ancho",
+            "province": "Madrid",
+            "municipality": "La Moraleja",
+            "district": "La Moraleja",
+            "country": "es",
+            "latitude": 40.5190225,
+            "longitude": -3.6214439,
+            "showAddress": false,
+            "url": "https://www.idealista.com/inmueble/90075076/",
+            "distance": "12016",
+            "description": "Venta de mansión en La Moraleja. Gran chalet localizado en zona céntrica de La Moraleja con varias construcciones anexas, garaje acristalado y bella parcela ajardinada con piscina. Pozo. Vivienda de unos 1500 metros cuadrados sobre bonita parcela de 10.000 metros cuadrados. Seis dormitorios con seis baños dentro de la construcción principal y dos dormitorios de servicio. Espacio adicional en construcciones anexas. Excelente ubicación. Probablemente La Moraleja sea la urbanización residencial de lujo más conocida de España. Los muchos personajes famosos que la han elegido como residencia habitual son responsables de su importante presencia mediática. Situada al norte de Madrid, en el término municipal de Alcobendas, tiene una extensión de 950 hectáreas. Cuenta con 1050 viviendas independientes sobre parcelas de 1.500, 2.500 y 10.000 m2 y con 400 adosados en 9 urbanizaciones diferentes. Tanto su estratégica ubicación -a un paso del centro de la capital (12km) y a escasos kilómetros del aeropuerto de Barajas (5km)- como sus férreas medidas de seguridad y sus muchas opciones de escolarización privada, ocio, deporte y salud, han convertido La Moraleja en un referente residencial en nuestro país.  Junto a las urbanizaciones de El Soto y El Encinar, conforman un núcleo geográfico de similares características demográficas. Su código postal es el 28109. Es aquí, en la plaza de La Moraleja (junto a la farmacia), donde Promora atiende a los clientes que quieren acceder a las viviendas de la zona norte de Madrid. En Idealista sólo podrá ver algunos de nuestros inmuebles, entre en nuestra web y conozca toda nuestra oferta.",
+            "hasVideo": true,
+            "status": "renew",
+            "newDevelopment": false,
+            "parkingSpace": {
+                "hasParkingSpace": true,
+                "isParkingSpaceIncludedInPrice": true
+            },
+            "priceByArea": 4500,
+            "detailedType": {
+                "typology": "chalet",
+                "subTypology": "independantHouse"
+            },
+            "suggestedTexts": {
+                "subtitle": "La Moraleja, La Moraleja",
+                "title": "Casa independiente en Calle del Camino Ancho"
+            },
+            "hasPlan": true,
+            "has3DTour": true,
+            "has360": false,
+            "hasStaging": true,
+            "labels": [
+                {
+                    "name": "villaType",
+                    "text": "Villa"
+                }
+            ],
+            "topNewDevelopment": false
+        },
+        {
+            "propertyCode": "96809828",
+            "thumbnail": "https://img3.idealista.com/blur/WEB_LISTING/0/id.pro.es.image.master/17/86/b9/959951384.jpg",
+            "externalReference": "SG168937",
+            "numPhotos": 37,
+            "floor": "1",
+            "price": 1120000,
+            "propertyType": "flat",
+            "operation": "sale",
+            "size": 249,
+            "exterior": true,
+            "rooms": 4,
+            "bathrooms": 3,
+            "address": "Paseo de la Castellana",
+            "province": "Madrid",
+            "municipality": "Madrid",
+            "district": "Tetuán",
+            "country": "es",
+            "neighborhood": "Cuzco-Castillejos",
+            "latitude": 40.4581266,
+            "longitude": -3.6914204,
+            "showAddress": false,
+            "url": "https://www.idealista.com/inmueble/96809828/",
+            "distance": "3252",
+            "description": "Gilmar Consulting Inmobiliario, oficina Castellana - Orense, vende excelente vivienda en pleno paseo de la castellana - cuzco. Vivienda para reformar con mucha fachada a castellana y en pleno cuzco. Dispone de una altura superior a un segundo piso.  La vivienda tiene más de 240m2 construidos aproximadamente y tiene la posibilidad de ser oficina.  Finca de construcción moderna en perfecto estado, primera planta, con dos ascensores. Magníficos servicios, cuenta con conserje, calefacción y agua caliente central, recogida de basuras, etc.  Plazas de garaje en las proximidades tanto en venta como alquiler.  A cinco minutos de la Universidad Pontificia de Comillas o por ejemplo del Campus de ESADE, o centros de Exposiciones como el de Arte Canal en el parque del Canal de Isabel II a 100m. Gracias a la perfecta localización, dispone de tranquilidad suficiente y a la vez unas magníficas comunicaciones. Bien hacia el exterior de Madrid por carretera, como para la movilidad interna vía Metro o Bus con paradas casi en la puerta. Al pie del edificio tenemos los mejores comercios, tiendas y todo lo que se necesita para cubrir las necesidades básicas y de ocio.  Es sin duda, una oportunidad única de vivir o invertir en una de las mejores zonas de Madrid. En Gilmar trabajamos para ofrecer a nuestros clientes los mejores inmuebles de Madrid, con equipos especializados en cada zona para proporcionarles la mejor opción a su necesidad, con todas las excelencias y cuidando al máximo los detalles.",
+            "hasVideo": true,
+            "status": "renew",
+            "newDevelopment": false,
+            "hasLift": true,
+            "priceByArea": 4498,
+            "detailedType": {
+                "typology": "flat"
+            },
+            "suggestedTexts": {
+                "subtitle": "Cuzco-Castillejos, Madrid",
+                "title": "Piso en Paseo de la Castellana"
+            },
+            "hasPlan": true,
+            "has3DTour": true,
+            "has360": false,
+            "hasStaging": false,
+            "topNewDevelopment": false
+        },
+        {
+            "propertyCode": "95952031",
+            "thumbnail": "https://img3.idealista.com/blur/WEB_LISTING/0/id.pro.es.image.master/0e/45/ef/932635384.jpg",
+            "externalReference": "DS-P77186",
+            "numPhotos": 38,
+            "floor": "1",
+            "price": 1190000,
+            "propertyType": "flat",
+            "operation": "sale",
+            "size": 160,
+            "exterior": true,
+            "rooms": 3,
+            "bathrooms": 3,
+            "address": "Calle de Rafael Calvo s/n",
+            "province": "Madrid",
+            "municipality": "Madrid",
+            "district": "Chamberí",
+            "country": "es",
+            "neighborhood": "Almagro",
+            "latitude": 40.4339148,
+            "longitude": -3.6949157,
+            "showAddress": true,
+            "url": "https://www.idealista.com/inmueble/95952031/",
+            "distance": "740",
+            "description": "DIZA Consultores les presenta piso totalmente exterior haciendo esquina con seis balcones, reformado a estrenar, de tres dormitorios en el corazón de Chamberí. En la primera planta de un edificio clásico con soberbio portal y ascensor de reja, construido a mediados de los años XX, encontramos este piso exterior con seis balcones de 152 metros cuadrados. Se distribuye en recibidor grande, salón-comedor amplio con cocina americana totalmente equipada, tres dormitorios, dos de ellos exteriores y con baño en suite y otro baño que comparten el dormitorio secundario. La cocina está completamente equipada con electrodomésticos de gama alta. Para ganar sensación de amplitud y luminosidad, la cocina está abierta con isla y el recibidor se divide con una estructura de cristal. Las calidades de la reforma son de primera calidad, con puertas lacadas, aire acondicionado por conductos, entre otros. El barrio de Almagro, perteneciente al distrito de Chamberí, se trata de uno de los más lujosos y tranquilos de Madrid. Cerca de los mejores restaurantes, tiendas, colegios y universidades de la ciudad, es uno de los mejores barrios para vivir en la capital. Estaremos encantados de atenderle. DIZA Consultores Inmobiliarios Inmobiliaria de lujo en Madrid especializada en comprar, vender y alquilar pisos en las zonas premium de Madrid: Barrio de Salamanca, Almagro, Chamartín, Retiro, Pozuelo, Aravaca, Boadilla. Convertimos Sueños en Hogares.",
+            "hasVideo": true,
+            "status": "good",
+            "newDevelopment": false,
+            "hasLift": true,
+            "parkingSpace": {
+                "hasParkingSpace": true,
+                "isParkingSpaceIncludedInPrice": false,
+                "parkingSpacePrice": 150
+            },
+            "priceByArea": 7438,
+            "detailedType": {
+                "typology": "flat"
+            },
+            "suggestedTexts": {
+                "subtitle": "Almagro, Madrid",
+                "title": "Piso en Calle de Rafael Calvo s/n"
+            },
+            "hasPlan": true,
+            "has3DTour": true,
+            "has360": false,
+            "hasStaging": true,
+            "labels": [
+                {
+                    "name": "apartamentoType",
+                    "text": "Apartamento"
+                },
+                {
+                    "name": "luxuryType",
+                    "text": "Lujo"
+                }
+            ],
+            "topNewDevelopment": false
+        },
+    ]
+}
