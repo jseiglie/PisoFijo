f11863160e16823af7dac375631ef7b4242e3cc2
jseiglie <javierseiglie@gmail.com>
2022-03-11 14:29:56 +0000

    testing db

--- migrations/versions/95f052321bba_.py
+++ migrations/versions/95f052321bba_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8f1093f197b7
+Revision ID: 95f052321bba
 Revises: 
-Create Date: 2022-03-11 13:42:30.992062
+Create Date: 2022-03-11 14:27:24.829766
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8f1093f197b7'
+revision = '95f052321bba'
 down_revision = None
 branch_labels = None
 depends_on = None
