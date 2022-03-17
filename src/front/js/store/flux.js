b41db9e166f46daa0d819b5e83c6bf23b61b0e36
Programisto1011 <victormaynougomez@gmail.com>
2022-03-17 15:45:41 +0000

    Fix error request idealista

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -32,76 +32,75 @@ const getState = ({ getStore, getActions, setStore }) => {
         	"https://3001-programisto1011-4geekaca-u47m9x84lcr.ws-eu38.gitpod.io/api/login",
 		baseUrlSearch:
 			"https://3001-programisto1011-4geekaca-u47m9x84lcr.ws-eu38.gitpod.io/api/search",
-      token: null,
-	  response: {},
-      country: "es", //(string) - values: es, it, pt (requiered)
-				filterUrl: "https://api.idealista.com/3.5/es/search?operation=sale&propertyType=homes&center=40.430,-3.702&distance=15000",
-				accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWFkIl0sImV4cCI6MTY0Njg4NzE4OSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9QVUJMSUMiXSwianRpIjoiMzcwM2Q3MmEtZjZjNi00N2U2LWIyZDQtNzEzNzMwZjAyOTc2IiwiY2xpZW50X2lkIjoidnI5ZHR0cGd2amZtaTVpazEyZGlvcDd1dXhrMDZlYWkifQ.IX-s6zVpi5aq_bxRPNONnfgz-NAFzDUnKpbs0-R-mjo",
-					//Example: https://api.idealista.com/3/es/search?locale=es&maxItems=20&numPage=1&operation=sale&
-					//order=publicationDate&propertyType=garages&sort=desc&apikey={api_key}&t=1&language=es&bankOffer=true&
-					//locationId=0-EU-ES-28
-				distanceRequest: 15000,
-				centerRequest: {lat:40.430, lng:-3.702},
-				filters:{
-					//generalFilters: 
-					operation: "sale", //(string) - values: sale, rent (requiered)
-					propertyType: "homes", //(string) - values: homes, offices, premises, garages, bedrooms (required)
-					center: "40.123,-3.242", //(string) - geographic coordinates (WGS84) (latitude,longitude)
-					locale: "es", //(string) - search language for summary - values: es, it, pt, en, ca
-					distance: 10500, //(double) - distance to center, in metres (ratio)
-					locationId: "", //(string) - idealista location code
-					maxPrice: 200000, //(double) - maximun price in response
-					minPrice: 50000, //(double) - minimun price in response
-					sinceDate: "W", //property age - W:last week, M: last month, T:last day (for rent except rooms), Y: last 2 days (sale and rooms) homeFilters: 
-					// homeFilters: 
-					minSize: 60, //double min size (from 60 m2 to 1000m2)
-					maxSize: 200,//double maxSize (from 60 m2 to 1000m2)
-					virtualTour: false,
-					flat: true, //boolean property is a flat
-					penthouse: false, //boolean
-					duplex: false, //boolean
-					studio: false, //boolean
-					chalet: false, //boolean
-					countryHouse: false, //boolean
-					bedrooms: "3", //(string) bedroom number (multivalued field) 0,1,2,3,4: bedroom number separated by commas. examples: "0", "1,4", "0,3", "0,2,4". 4 means "4 or more"
-					bathrooms: "3", //(string) bathroom number 0,1,2,3: , bedroom number separated by commas. examples: "0", "0,3", "0,2,3". 3 means "3 or more"
-					preservation: "good", //(string) - property preservation - values: good, renew
-					bankOffer: false, //owner is a bank - works for sale in spain
-					elevator: true //(boolean)
-				},
-				inputLocation: {address: "barcelona"},
-				propertiesSearch: exampleRequestIdealista.elementList,
-				selected: [],
-				listFavorites: [],
-				optionsArr: ["flat", "penthouse", "duplex", "studio", "chalet", "countryHouse"]
+      	token: null,
+      	country: "es", //(string) - values: es, it, pt (requiered)
+		filterUrl: "https://api.idealista.com/3.5/es/search?operation=sale&propertyType=homes&center=40.430,-3.702&distance=15000",
+		accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWFkIl0sImV4cCI6MTY0Njg4NzE4OSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9QVUJMSUMiXSwianRpIjoiMzcwM2Q3MmEtZjZjNi00N2U2LWIyZDQtNzEzNzMwZjAyOTc2IiwiY2xpZW50X2lkIjoidnI5ZHR0cGd2amZtaTVpazEyZGlvcDd1dXhrMDZlYWkifQ.IX-s6zVpi5aq_bxRPNONnfgz-NAFzDUnKpbs0-R-mjo",
+			//Example: https://api.idealista.com/3/es/search?locale=es&maxItems=20&numPage=1&operation=sale&
+			//order=publicationDate&propertyType=garages&sort=desc&apikey={api_key}&t=1&language=es&bankOffer=true&
+			//locationId=0-EU-ES-28
+		distanceRequest: 15000,
+		centerRequest: {lat:40.430, lng:-3.702},
+		filters:{
+			//generalFilters: 
+			operation: "sale", //(string) - values: sale, rent (requiered)
+			propertyType: "homes", //(string) - values: homes, offices, premises, garages, bedrooms (required)
+			center: "40.123,-3.242", //(string) - geographic coordinates (WGS84) (latitude,longitude)
+			locale: "es", //(string) - search language for summary - values: es, it, pt, en, ca
+			distance: 10500, //(double) - distance to center, in metres (ratio)
+			locationId: "", //(string) - idealista location code
+			maxPrice: 200000, //(double) - maximun price in response
+			minPrice: 0, //(double) - minimun price in response
+			sinceDate: "M", //property age - W:last week, M: last month, T:last day (for rent except rooms), Y: last 2 days (sale and rooms) homeFilters: 
+			// homeFilters: 
+			minSize: 30, //double min size (from 60 m2 to 1000m2)
+			maxSize: 500,//double maxSize (from 60 m2 to 1000m2)
+			virtualTour: false,
+			flat: true, //boolean property is a flat
+			penthouse: false, //boolean
+			duplex: false, //boolean
+			studio: false, //boolean
+			chalet: true, //boolean
+			countryHouse: false, //boolean
+			bedrooms: "", //(string) bedroom number (multivalued field) 0,1,2,3,4: bedroom number separated by commas. examples: "0", "1,4", "0,3", "0,2,4". 4 means "4 or more"
+			bathrooms: "", //(string) bathroom number 0,1,2,3: , bedroom number separated by commas. examples: "0", "0,3", "0,2,3". 3 means "3 or more"
+			preservation: "good", //(string) - property preservation - values: good, renew
+			bankOffer: false, //owner is a bank - works for sale in spain
+			elevator: false //(boolean)
+		},
+		inputLocation: {address: "barcelona"},
+		propertiesSearch: exampleRequestIdealista.elementList,
+		selected: [],
+		listFavorites: [],
+		optionsArr: ["flat", "penthouse", "duplex", "studio", "chalet", "countryHouse"]
     },
     actions: {
-      addElementListArr: (inputValue) => {
-        setStore({ list: [...getStore().list, inputValue] });
-      },
+		addElementListArr: (inputValue) => {
+			setStore({ list: [...getStore().list, inputValue] });
+		},
 
      //---------------------------------------------------------------------------------------------
-			handleChange: (e, sectionStore) => {
+		handleChange: (e, sectionStore) => {
 				const {name, value} = e.target;
 				console.log(`name: ${name}`, `value: ${value}`)
 				setStore({sectionStore:{...getStore().sectionStore, [name]: value}})
 				console.log(`${sectionStore} change input: `, getStore().sectionStore);
 				console.log("filters change input: ", getStore().filters);
-			},
-			//----------------------------------------------------------------------------------------------
+		},
+		//----------------------------------------------------------------------------------------------
 
-			handleChangeRadio: e => {
+		handleChangeRadio: e => {
 				const {name, value} = e.target;
 				if(e.target.checked){
 					setStore({filters:{...getStore().filters, [name]: value}});
 					console.log("filters change radio: ", getStore().filters);
 				}
-			},
-			//----------------------------------------------------------------------------------------------
+		},
+		//----------------------------------------------------------------------------------------------
 
-			//VICTOR - Habría que refactorizar para que no haya que introducir las opciones
+		//VICTOR - Habría que refactorizar para que no haya que introducir las opciones
 
-			handleChangeSelected: (e, optionsArr) => {
+		handleChangeSelected: (e, optionsArr) => {
 				const {value} = e.target;
 				optionsArr.map((option) => {
 					if(option == value){
@@ -112,15 +111,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}       	     
 				})
 				console.log("filters change selected: ", getStore().filters); 
-			},
-			//-------------------------------------------------------------------------------------------------
-
-			transformAddressToLanLong: (address) => {
+		},
+		//-------------------------------------------------------------------------------------------------
+		transformAddressToLanLong: (address) => {
 				setStore({filters:{...getStore().filters, "center": getActions().getLatLonByAddress(address)}})
 				console.log("filters after transform center: ", getStore().filters);
-			},
-
-			getLatLonByAddress: addressText =>{
+		},
+		getLatLonByAddress: addressText =>{
 				Geocode.fromAddress(addressText).then(
 					(response) => {
 					  const { lat, lng } = response.results[0].geometry.location;
@@ -131,38 +128,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 					(error) => {
 					  console.error(error);
 					}
-				  );	  
-			},
-
-			// Input:{country: "es", operation: "sale"} 
-			// Output: [["country", "es"], ["operation","sale"]]
-
-			filterEntries: filters =>Object.entries(filters), 
-			filteredArrElementsNotEmpty: arr =>{
-				return arr.filter(el => el[1] != '' || el[1] == true)
-			},
-			concatenateArr: (arr)=>{
-				return ((arr.map(el =>el.join("="))).join("&"))
-			},
-			UrlFilters: filtersObj =>{
+				);	  
+		},
+		// Input:{country: "es", operation: "sale"} 
+		// Output: [["country", "es"], ["operation","sale"]]
+		filterEntries: filters =>Object.entries(filters), 
+		filteredArrElementsNotEmpty: arr =>{
+			return arr.filter(el => el[1] != '' || el[1] == true)
+		},
+		concatenateArr: (arr)=>{
+			return ((arr.map(el =>el.join("="))).join("&"))
+		},
+		UrlFilters: filtersObj =>{
 				const url = getActions().concatenateArr(
 							getActions().filteredArrElementsNotEmpty(
 							getActions().filterEntries(filtersObj)));
 				console.log("UrlFilters: ",url)
 				return (url)
-			},
-			//Input: {operation: "sale", center: "40.123,-3.242", ...} 
-			//Output: "operation=sale&center=40.123,-3.242&locale=es&distance=3500&maxPrice=200000&minPrice=50000&sinceDate=W"
-
-			getSelectedProperty: (selectedProperty) =>{
+		},
+		//Input: {operation: "sale", center: "40.123,-3.242", ...} 
+		//Output: "operation=sale&center=40.123,-3.242&locale=es&distance=3500&maxPrice=200000&minPrice=50000&sinceDate=W"
+		getSelectedProperty: (selectedProperty) =>{
 				setStore({selected: selectedProperty})
-			},
-
-			getFilterUrl: url =>{
+		},
+		getFilterUrl: url =>{
 				setStore({filterUrl: url})
-			},
-
-			getAccessToken: () => {
+		},
+		getAccessToken: () => {
 				console.log(" aqui urlRequestTokenAPI",urlRequestTokenAPI)
 				fetch(urlRequestTokenAPI,{
 					method: "POST",
@@ -185,9 +177,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}).catch(err =>{
 						console.error(err.message)
 					})
-					},
-
-			getDetailsOfProperties: () => {
+				},
+		getDetailsOfProperties: () => {
 
 				var myHeaders = new Headers();
 				myHeaders.append("Content-Type", "application/json");
@@ -204,9 +195,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 				.then(response => response.text())
 				.then(result => console.log(result))
 				.catch(error => console.log('error', error));
-			},
-
-			getDetailsOfPropertiesTest: (filterUrl) => {
+		},
+		getDetailsOfPropertiesTest: (filterUrl) => {
 			 	let url = urlBaseAPI.concat(getStore().country,"/search?",filterUrl);
 				// let url = "https://api.idealista.com/3.5/es/search?operation=sale&propertyType=homes&center=40.430,-3.702&distance=15000"
 				console.log("URL fetch: ",url);
@@ -226,90 +216,91 @@ const getState = ({ getStore, getActions, setStore }) => {
 				.then(response => response.text())
 				.then(result => console.log(result))
 				.catch(error => console.log('error', error));		
-			},
-			search: async (data) => { //<-----------------------------------------------------------------------
-				const dato = {url:"operation=sale&propertyType=homes&center=40.430,-3.702&distance=15000"}
-				// const dato = getStore().filters
-				console.log("data en flux", data);
-				const resp = await fetch(getStore().baseUrlSearch, {
-				  method: "POST",
-				  headers: {
-					"Content-Type": "application/json",
-					Accept: "application'json",
-				  },
-				  body: JSON.stringify(dato),
-				});
-				if (resp.ok) {
-					console.log("resp", resp)
-					
-				  const dataSearch = await resp.json();
-				  console.log("respues en el flux", dataSearch)
-				 	setStore({response: dataSearch}); //<-----------------------------------------------------------------------
-					console.log("Output API: ", getStore().response);
-				} else {
-				  alert("ALGO FALLO");
-				}
+		},
+		search: async (data) => { //<-----------------------------------------------------------------------
+			const dato = {url:"operation=sale&propertyType=homes&center=40.430,-3.702&distance=15000"}
+			// const dato = getStore().filters
+			console.log("data en flux", data);
+			const resp = await fetch(getStore().baseUrlSearch, {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+				Accept: "application'json",
 			  },
+			  body: JSON.stringify(data),
+			});
+			if (resp.ok) {
+				// console.log("resp", resp)
+				
+			  const dataSearch = await resp.json();
+			//   console.log("respues en el flux", dataSearch)
+			 	setStore({response: dataSearch}); //<-----------------------------------------------------------------------
+				console.log("Output API: ", getStore().propertiesSearch);
+				console.log("latitude",getStore().propertiesSearch.elementList)
+			} else {
+			  alert("ALGO FALLO");
+			}
+		},
 
-      login: async (email, password) => {
-        const opts = {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-			Accept: "application'json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        };
-        try {
-          const resp = await 
-			fetch(getStore().baseUrlLogin,
-            opts
-          );
-          if (resp.status !== 200) {
-            alert("There has been some error");
-            return false;
-          }
-          const data = await resp.json();
-          sessionStorage.setItem("token", data.access_token);
-          setStore({ token: data.access_token });
-          return true;
-        } catch (error) {
-          console.error("There's has been an error login in");
-        }
-      },
+      	login: async (email, password) => {
+			const opts = {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application'json",
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+			};
+			try {
+			const resp = await 
+				fetch(getStore().baseUrlLogin,
+				opts
+			);
+			if (resp.status !== 200) {
+				alert("There has been some error");
+				return false;
+			}
+			const data = await resp.json();
+			sessionStorage.setItem("token", data.access_token);
+			setStore({ token: data.access_token });
+			return true;
+			} catch (error) {
+			console.error("There's has been an error login in");
+			}
+		},
 
-      logOut: () => {
-        sessionStorage.removeItem("token");
-        setStore({ token: null });
-      },
-      syncTokenFromSessionStore: () => {
-        const token = sessionStorage.getItem("token");
-        if (token && token != "" && token != undefined)
-          setStore({ token: token });
-      },
-      register: async (data) => {
-        console.log("data en flux", data);
-        const resp = await fetch(getStore().baseUrlRegister, {
-          method: "POST",
-		  
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application'json",
-          },
-          body: JSON.stringify(data),
-        });
-        if (resp.status == 201) {
-          const data = await resp.json();
-		  sessionStorage.setItem("token", data.access_token);
-          setStore({ token: data.access_token });
-        } else {
-          alert("User already exist, try logging in");
-        }
-      },
+		logOut: () => {
+			sessionStorage.removeItem("token");
+			setStore({ token: null });
+		},
+		syncTokenFromSessionStore: () => {
+			const token = sessionStorage.getItem("token");
+			if (token && token != "" && token != undefined)
+			setStore({ token: token });
+		},
+		register: async (data) => {
+			console.log("data en flux", data);
+			const resp = await fetch(getStore().baseUrlRegister, {
+			method: "POST",
+			
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application'json",
+			},
+			body: JSON.stringify(data),
+			});
+			if (resp.status == 201) {
+				const data = await resp.json();
+				sessionStorage.setItem("token", data.access_token);
+				setStore({ token: data.access_token });
+			} else {
+				alert("User already exist, try logging in");
+			}
+		},
     },
   };
 };
